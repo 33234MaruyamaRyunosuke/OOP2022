2beac137d8013431f835bd221be1171c122bdcc3
33234MaruyamaRyunosuke <33234@ojs.ac.jp>
2022-05-10 15:20:42 +0900

    問題2.1.3完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -10,11 +10,20 @@ namespace Exercise {
 
         static void Main(string[] args) {
 
-            //PrintSongs(Songs);
+            var songs = new Song[] {
+                new Song("タイトル1", "アーティストa", 243),
+                new Song("タイトル2", "アーティストb", 226),
+                new Song("タイトル3", "アーティストc", 304),
+                new Song("タイトル4", "アーティストc", 280),
+                new Song("タイトル5", "アーティストe", 319),
+            };
+            PrintSongs(songs);
         }
 
         private static void PrintSongs(Song[] songs) {
+            foreach(var song in songs) {
 
+            }
         }
     }
 }
