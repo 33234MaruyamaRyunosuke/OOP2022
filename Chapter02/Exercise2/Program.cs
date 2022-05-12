c448a7b6d94f1932d2e231881184a6f6f8cdc52c
33234MaruyamaRyunosuke <33234@ojs.ac.jp>
2022-05-12 14:16:31 +0900

    Distanceコンバーターを流用

---
+++ Chapter02/Exercise2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+
+            if (args.Length >= 1 && args[0] == "-tom") {
+                //インチからメートルへの対応表を出力
+                PrintInchToMeterList(1, 10);
+            }
+            else {
+                //メートルからインチへの対応表を出力
+                PrintMeterToInchList(1, 10);
+            }
+
+        }
+
+        //インチからメートルへの対応表を出力
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int Inch = 1; Inch <= stop; Inch++) {
+                double meter = InchConverter.ToMeter(Inch);
+                Console.WriteLine("{0} ih = {1:0.0000} m", Inch, meter);
+            }
+        }
+
+        //メートルからインチへの対応表を出力
+        private static void PrintMeterToInchList(int start, int stop) {
+            for (int meter = 1; meter <= 10; meter++) {
+                double Inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ih", meter, Inch);
+            }
+        }
+    }
+}
