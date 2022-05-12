f7cca1e613e2a7f368413afdf7f5ba97fbbfd871
33234MaruyamaRyunosuke <33234@ojs.ac.jp>
2022-05-12 14:47:06 +0900

    SalesCalculator流用

---
+++ Chapter02/Exercise3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) { 
+            var sales = new SelesCounter("sales.csv");
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
+                Console.WriteLine("{0}{1}", obj.Key, obj.Value);
+            }
+        }
+    }
+}
---
+++ Chapter02/Exercise3/SelesCounter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    public class SelesCounter {
+        private IEnumerable<Sale> _sales; //csvファイルから読み込んだデータ
+
+        //コンストラクタ
+        public SelesCounter(string filePath) {
+            _sales = ReadSales(filePath);
+        }
+
+        //店舗別売り上げを求める
+        public IDictionary<String, int> GetPerStoreSales() {
+            var dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;
+                else
+                    dict[sale.ShopName] = sale.Amount;
+            }
+            return dict;
+        }
+
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static IEnumerable<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] Lines = File.ReadAllLines(filePath);
+            foreach (string Line in Lines) {
+                string[] items = Line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    productCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+    }
+}
