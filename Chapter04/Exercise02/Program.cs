bc10effb7964a050e80b8de575508bccc89d5743
33234MaruyamaRyunosuke <33234@ojs.ac.jp>
2022-05-24 13:27:42 +0900

    問題4.2作成準備

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -1,18 +1,21 @@
-﻿using System;
+﻿using Exercise01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise01 {
+namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
-            var ym = new YearMonth(2022, 5);
+
+
+            /*var ym = new YearMonth(2022, 5);
 
             var check = ym.Is21Century;
             var test = ym.AddOneMonth();
 
-            Console.WriteLine(ym.ToString());
+            Console.WriteLine(ym);*/
         }
     }
 }
