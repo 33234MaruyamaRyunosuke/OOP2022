8ff4b1dcc59c1c08e44343390b76ccd378c345b1
33234MaruyamaRyunosuke <104185425+33234MaruyamaRyunosuke@users.noreply.github.com>
2022-06-09 15:00:42 +0900

    画面デザイン完了

--- FormAppSample/AddressBook/Form1.Designer.cs
+++ FormAppSample/AddressBook/Form1.Designer.cs
@@ -24,13 +24,254 @@ namespace AddressBook {
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvPersons = new System.Windows.Forms.DataGridView();
+            this.tbname = new System.Windows.Forms.TextBox();
+            this.tbMailAddress = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.tbtbCompany = new System.Windows.Forms.TextBox();
+            this.cbFamily = new System.Windows.Forms.CheckBox();
+            this.cbFriend = new System.Windows.Forms.CheckBox();
+            this.cbWork = new System.Windows.Forms.CheckBox();
+            this.cbOther = new System.Windows.Forms.CheckBox();
+            this.btAddPerson = new System.Windows.Forms.Button();
+            this.pbPicture = new System.Windows.Forms.PictureBox();
+            this.btPictrureOpen = new System.Windows.Forms.Button();
+            this.btPictureClear = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersons)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(139, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "名前";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(49, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "メールアドレス";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(139, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "住所";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(139, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "会社";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(108, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 27);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "グループ";
+            // 
+            // dgvPersons
+            // 
+            this.dgvPersons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersons.Location = new System.Drawing.Point(43, 327);
+            this.dgvPersons.Name = "dgvPersons";
+            this.dgvPersons.RowTemplate.Height = 21;
+            this.dgvPersons.Size = new System.Drawing.Size(763, 150);
+            this.dgvPersons.TabIndex = 5;
+            // 
+            // tbname
+            // 
+            this.tbname.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbname.Location = new System.Drawing.Point(246, 32);
+            this.tbname.Name = "tbname";
+            this.tbname.Size = new System.Drawing.Size(329, 31);
+            this.tbname.TabIndex = 6;
+            // 
+            // tbMailAddress
+            // 
+            this.tbMailAddress.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbMailAddress.Location = new System.Drawing.Point(246, 82);
+            this.tbMailAddress.Name = "tbMailAddress";
+            this.tbMailAddress.Size = new System.Drawing.Size(329, 31);
+            this.tbMailAddress.TabIndex = 7;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbAddress.Location = new System.Drawing.Point(246, 133);
+            this.tbAddress.Multiline = true;
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(329, 60);
+            this.tbAddress.TabIndex = 8;
+            // 
+            // tbtbCompany
+            // 
+            this.tbtbCompany.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbtbCompany.Location = new System.Drawing.Point(246, 219);
+            this.tbtbCompany.Name = "tbtbCompany";
+            this.tbtbCompany.Size = new System.Drawing.Size(329, 31);
+            this.tbtbCompany.TabIndex = 9;
+            // 
+            // cbFamily
+            // 
+            this.cbFamily.AutoSize = true;
+            this.cbFamily.Location = new System.Drawing.Point(246, 290);
+            this.cbFamily.Name = "cbFamily";
+            this.cbFamily.Size = new System.Drawing.Size(48, 16);
+            this.cbFamily.TabIndex = 10;
+            this.cbFamily.Text = "家族";
+            this.cbFamily.UseVisualStyleBackColor = true;
+            // 
+            // cbFriend
+            // 
+            this.cbFriend.AutoSize = true;
+            this.cbFriend.Location = new System.Drawing.Point(335, 290);
+            this.cbFriend.Name = "cbFriend";
+            this.cbFriend.Size = new System.Drawing.Size(48, 16);
+            this.cbFriend.TabIndex = 11;
+            this.cbFriend.Text = "友人";
+            this.cbFriend.UseVisualStyleBackColor = true;
+            // 
+            // cbWork
+            // 
+            this.cbWork.AutoSize = true;
+            this.cbWork.Location = new System.Drawing.Point(413, 290);
+            this.cbWork.Name = "cbWork";
+            this.cbWork.Size = new System.Drawing.Size(48, 16);
+            this.cbWork.TabIndex = 12;
+            this.cbWork.Text = "仕事";
+            this.cbWork.UseVisualStyleBackColor = true;
+            // 
+            // cbOther
+            // 
+            this.cbOther.AutoSize = true;
+            this.cbOther.Location = new System.Drawing.Point(493, 290);
+            this.cbOther.Name = "cbOther";
+            this.cbOther.Size = new System.Drawing.Size(55, 16);
+            this.cbOther.TabIndex = 13;
+            this.cbOther.Text = "その他";
+            this.cbOther.UseVisualStyleBackColor = true;
+            // 
+            // btAddPerson
+            // 
+            this.btAddPerson.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btAddPerson.Location = new System.Drawing.Point(625, 265);
+            this.btAddPerson.Name = "btAddPerson";
+            this.btAddPerson.Size = new System.Drawing.Size(74, 54);
+            this.btAddPerson.TabIndex = 14;
+            this.btAddPerson.Text = "追加";
+            this.btAddPerson.UseVisualStyleBackColor = true;
+            // 
+            // pbPicture
+            // 
+            this.pbPicture.Location = new System.Drawing.Point(625, 36);
+            this.pbPicture.Name = "pbPicture";
+            this.pbPicture.Size = new System.Drawing.Size(155, 157);
+            this.pbPicture.TabIndex = 15;
+            this.pbPicture.TabStop = false;
+            // 
+            // btPictrureOpen
+            // 
+            this.btPictrureOpen.Location = new System.Drawing.Point(625, 210);
+            this.btPictrureOpen.Name = "btPictrureOpen";
+            this.btPictrureOpen.Size = new System.Drawing.Size(75, 23);
+            this.btPictrureOpen.TabIndex = 16;
+            this.btPictrureOpen.Text = "開く...";
+            this.btPictrureOpen.UseVisualStyleBackColor = true;
+            // 
+            // btPictureClear
+            // 
+            this.btPictureClear.Location = new System.Drawing.Point(705, 210);
+            this.btPictureClear.Name = "btPictureClear";
+            this.btPictureClear.Size = new System.Drawing.Size(75, 23);
+            this.btPictureClear.TabIndex = 17;
+            this.btPictureClear.Text = "クリア";
+            this.btPictureClear.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(845, 492);
+            this.Controls.Add(this.btPictureClear);
+            this.Controls.Add(this.btPictrureOpen);
+            this.Controls.Add(this.pbPicture);
+            this.Controls.Add(this.btAddPerson);
+            this.Controls.Add(this.cbOther);
+            this.Controls.Add(this.cbWork);
+            this.Controls.Add(this.cbFriend);
+            this.Controls.Add(this.cbFamily);
+            this.Controls.Add(this.tbtbCompany);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.tbMailAddress);
+            this.Controls.Add(this.tbname);
+            this.Controls.Add(this.dgvPersons);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersons)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvPersons;
+        private System.Windows.Forms.TextBox tbname;
+        private System.Windows.Forms.TextBox tbMailAddress;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.TextBox tbtbCompany;
+        private System.Windows.Forms.CheckBox cbFamily;
+        private System.Windows.Forms.CheckBox cbFriend;
+        private System.Windows.Forms.CheckBox cbWork;
+        private System.Windows.Forms.CheckBox cbOther;
+        private System.Windows.Forms.Button btAddPerson;
+        private System.Windows.Forms.PictureBox pbPicture;
+        private System.Windows.Forms.Button btPictrureOpen;
+        private System.Windows.Forms.Button btPictureClear;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
