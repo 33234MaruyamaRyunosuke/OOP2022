2c57b001e6ed997cf9f4cb9bba30fa2790f71135
33234MaruyamaRyunosuke <104185425+33234MaruyamaRyunosuke@users.noreply.github.com>
2022-07-04 11:58:32 +0900

    練習問題11.1-1

--- Chapter11/XmlSample/Program.cs
+++ Chapter11/XmlSample/Program.cs
@@ -10,22 +10,17 @@ namespace XmlSample {
     class Program {
         static void Main(string[] args) {
 
-            using (var wc = new WebClient()) {
-                
-                var stream = wc.OpenRead("https://news.yahoo.co.jp/rss/media/jomo/all.xml");
-
-                var xdoc = XDocument.Load(stream);
-                var xNews = xdoc.Root.Descendants("item").Select(x=>x.Element("title"));
-
-                foreach (var data in xNews) {
-                    Console.WriteLine(data);
-
-                }
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name"),
+                                    Birth = (DateTime)x.Element("birth"),
+                                    Death = (DateTime)x.Element("death")
+                                });
+            foreach (var novelist in novelists) {
+                Console.WriteLine("{0} ({1}-{2})",
+                                   novelist.Name, novelist.Birth.Year, novelist.Death.Year);
             }
-
-            
-
-            
         }
     }
 }
