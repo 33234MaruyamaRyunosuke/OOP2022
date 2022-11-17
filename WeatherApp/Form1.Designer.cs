a3692f969b9aba36475db44152a89335e6b19761
33234MaruyamaRyunosuke <104185425+33234MaruyamaRyunosuke@users.noreply.github.com>
2022-11-17 15:53:49 +0900

    WeatherApp 11/17

--- WeatherApp/Form1.Designer.cs
+++ WeatherApp/Form1.Designer.cs
@@ -28,15 +28,19 @@ namespace WeatherApp {
             this.tbWeatherInfo = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.cbRegion = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.pbWeatherIcon = new System.Windows.Forms.PictureBox();
+            this.cbRegion = new System.Windows.Forms.ComboBox();
+            this.tbPresenter = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbWeatherIcon)).BeginInit();
             this.SuspendLayout();
             // 
             // btWeatherGet
             // 
-            this.btWeatherGet.Location = new System.Drawing.Point(518, 140);
+            this.btWeatherGet.Location = new System.Drawing.Point(509, 142);
             this.btWeatherGet.Name = "btWeatherGet";
             this.btWeatherGet.Size = new System.Drawing.Size(111, 49);
             this.btWeatherGet.TabIndex = 0;
@@ -49,7 +53,7 @@ namespace WeatherApp {
             this.tbWeatherInfo.Location = new System.Drawing.Point(93, 195);
             this.tbWeatherInfo.Multiline = true;
             this.tbWeatherInfo.Name = "tbWeatherInfo";
-            this.tbWeatherInfo.Size = new System.Drawing.Size(536, 178);
+            this.tbWeatherInfo.Size = new System.Drawing.Size(289, 178);
             this.tbWeatherInfo.TabIndex = 1;
             // 
             // label1
@@ -64,24 +68,16 @@ namespace WeatherApp {
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(58, 140);
+            this.label2.Location = new System.Drawing.Point(58, 98);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 12);
             this.label2.TabIndex = 3;
             this.label2.Text = "地域";
             // 
-            // cbRegion
-            // 
-            this.cbRegion.FormattingEnabled = true;
-            this.cbRegion.Location = new System.Drawing.Point(93, 137);
-            this.cbRegion.Name = "cbRegion";
-            this.cbRegion.Size = new System.Drawing.Size(121, 20);
-            this.cbRegion.TabIndex = 4;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(46, 62);
+            this.label3.Location = new System.Drawing.Point(46, 55);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 12);
             this.label3.TabIndex = 5;
@@ -95,14 +91,60 @@ namespace WeatherApp {
             this.pbWeatherIcon.TabIndex = 6;
             this.pbWeatherIcon.TabStop = false;
             // 
+            // cbRegion
+            // 
+            this.cbRegion.FormattingEnabled = true;
+            this.cbRegion.Location = new System.Drawing.Point(93, 95);
+            this.cbRegion.Name = "cbRegion";
+            this.cbRegion.Size = new System.Drawing.Size(121, 20);
+            this.cbRegion.TabIndex = 7;
+            // 
+            // tbPresenter
+            // 
+            this.tbPresenter.Location = new System.Drawing.Point(93, 52);
+            this.tbPresenter.Name = "tbPresenter";
+            this.tbPresenter.Size = new System.Drawing.Size(121, 19);
+            this.tbPresenter.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(419, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "明日の天気";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(419, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "明後日の天気";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(419, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "週間予報";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(654, 404);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbPresenter);
+            this.Controls.Add(this.cbRegion);
             this.Controls.Add(this.pbWeatherIcon);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.cbRegion);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbWeatherInfo);
@@ -121,9 +163,13 @@ namespace WeatherApp {
         private System.Windows.Forms.TextBox tbWeatherInfo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox cbRegion;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pbWeatherIcon;
+        private System.Windows.Forms.ComboBox cbRegion;
+        private System.Windows.Forms.TextBox tbPresenter;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- WeatherApp/Form1.cs
+++ WeatherApp/Form1.cs
@@ -26,8 +26,19 @@ namespace WeatherApp {
             var json = JsonConvert.DeserializeObject<Rootobject>(dString);
 
             tbWeatherInfo.Text = json.text;
+            tbPresenter.Text = json.publishingOffice;
             cbRegion.Text = json.targetArea;
 
+            /*var now = DateTimeOffset.Now;
+            tbWeatherInfo.Text = now.ToString();
+            tbWeatherInfo.Text += "\r\n";*/
+
+            
+
+            pbWeatherIcon.ImageLocation = "https://www.jma.go.jp/bosai/forecast/img/101.png";
+
         }
+
+        
     }
 }
